5cfdacc71afe348d7a3f76d06c685e3262b0ecfc
yangjaehyuk <didwogur28@naver.com>
2022-09-21 13:57:10 +0900

    # feat : Lv1- 16, 17, 18

---
+++ src/main/java/study/Lv1_17.java
@@ -0,0 +1,67 @@
+package study;
+
+/*
+ * array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+ * divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 -1을 담아 반환하세요.
+ *
+ * 제한사항: arr은 자연수를 담은 배열입니다. / 정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다. / divisor는 자연수입니다. / array는 길이 1 이상인 배열입니다.
+ */
+
+import java.util.Arrays;
+
+public class Lv1_17 {
+
+    public static void main(String[] args) {
+
+        int[] solution = solution(new int[]{2, 36, 1, 3}, 1);
+
+        for(int i=0; i<solution.length; i++) {
+            System.out.println(solution[i]);
+        }
+
+    }
+
+    public static int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        int chk = 1;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] % divisor == 0) {
+                chk++;
+            }
+        }
+
+        if(chk==1) {
+            answer = new int[chk];
+            answer[0] = -1;
+            return answer;
+        }
+
+        answer = new int[chk-1];
+        chk = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] % divisor == 0) {
+                answer[chk] = arr[i];
+                chk++;
+            }
+        }
+
+        Arrays.sort(answer);
+        return answer;
+    }
+}
+
+
+/*
+
+    - 배열의 원소 가공
+     Arrays.stream(arr).filter(factor -> factor % divisor == 0).toArray();
+
+     > map: 요소들을 특정 조건에 해당하는 값으로 변환
+     > filter: 요소들을 조건에 따라 걸러줌
+     > sorted: 요소들을 정렬
+
+ */
+
+
---
+++ src/main/java/study/Lv1_18.java
@@ -0,0 +1,43 @@
+package study;
+
+/*
+ * 정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
+ * 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
+ * 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+ *
+ * 제한사항: arr은 길이 1 이상인 배열입니다. / 인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Lv1_18 {
+
+    public static void main(String[] args) {
+
+        int[] solution = solution(new int[]{4,3,2,1});
+
+        for(int i=0; i<solution.length; i++) {
+            System.out.println(solution[i]);
+        }
+
+    }
+
+    public static int[] solution(int[] arr) {
+
+        if(arr.length <= 1) return new int[]{-1};
+        return java.util.Arrays.stream(arr).filter(arg -> arg != java.util.Arrays.stream(arr).min().getAsInt()).toArray();
+
+    }
+}
+
+
+/*
+
+    - Arrays.stream(배열).min().getAsInt()
+     > 배열에서 가장 작은 요소를 Int형태로 반환
+
+ */
+
+
