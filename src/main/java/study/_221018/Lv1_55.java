264a4592246c3615facf6cc53654f97308b5f40e
yangjaehyuk <didwogur28@naver.com>
2022-10-19 12:50:46 +0900

    # feat : Lv1 - 55

--- src/main/java/study/_221018/Lv1_55.java
+++ src/main/java/study/_221018/Lv1_55.java
@@ -56,7 +56,7 @@ public class Lv1_55 {
 
     public static void main(String[] args) {
 
-        String solution = solution("..!$.-...-..");
+        String solution = solution("abcdefghijklmn.p");
 
         System.out.println(solution);
     }
@@ -65,40 +65,21 @@ public class Lv1_55 {
 
         String answer = "";
 
-        String pattern =  "[a-z0-9,\\-,_,.]";
-        String charPattern = "[a-z]";
-
-        int chkChar = 0;
-
         // case1 : new_id의 모든 대문자를 대응되는 소문자로 치환
-        new_id = new_id.toLowerCase(Locale.ROOT);
-
-        // case5 : new_id가 빈 문자열이라면, new_id에 "a"를 대입
-        for(int i=0; i<new_id.length(); i++) {
-            if(new_id.split("")[i].matches(charPattern)) {
-                chkChar++;
-            }
-        }
-        if(chkChar == 0) {
-
-            new_id = removeChar(new_id);
-            new_id = lengChk(new_id);
-
-            for(int i=0; i<new_id.length(); i++) {
-                answer += "a";
-            }
-
-            return answer;
-        }
-
-        answer = "";
+        answer = new_id.toLowerCase(Locale.ROOT);
 
         // case2 : new_id에서 알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자를 제거
+        answer = removeChar(answer);
+
         // case3 : new_id에서 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환
-        answer = removeChar(new_id);
+        answer = removeDupPeriod(answer);
 
         // case 4 : new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거
         answer = removePeriod(answer);
+
+        if(answer.length() == 0) {
+            answer += "a";
+        }
 
         // case 6 : new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거
         // case 7 : new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙임
@@ -107,81 +88,85 @@ public class Lv1_55 {
         return answer;
     }
 
-    public static String removeChar(String new_id) {
+    public static String removeChar(String arg) {
 
         String answer = "";
-        String pattern =  "[a-z0-9,\\-,_,.]";
-        int periodCnt = 0;
 
-        for(int i=0; i<new_id.split("").length; i++) {
+        for(int i=0; i<arg.length(); i++) {
 
-            if(new_id.split("")[i].matches(pattern)) {
+            char charSplit = arg.charAt(i);
 
-                if(new_id.split("")[i].equals(".")) {
-                    if(periodCnt == 0) {
-                        answer += new_id.split("")[i];
-                    }
-                    periodCnt ++;
-                } else {
-                    answer += new_id.split("")[i];
-                    periodCnt = 0;
-                }
+            if(charSplit >= 'a' && charSplit <= 'z') {
+                answer += String.valueOf(charSplit);
+            } else if(charSplit >= '0' && charSplit <= '9') {
+                answer += String.valueOf(charSplit);
+            } else if(charSplit == '.' || charSplit == '-' || charSplit == '_') {
+                answer += String.valueOf(charSplit);
             }
         }
 
         return answer;
     }
 
+    public static String removeDupPeriod(String arg) {
+
+        for(int i = 0; i < arg.length(); i++) {
+            if(arg.charAt(i) == '.') {
+                int j = i+1;
+                String dot = ".";
+
+                while(j != arg.length() && arg.charAt(j) == '.') {
+                    dot += ".";
+                    j++;
+                }
+
+                if(dot.length() > 1)
+                    arg = arg.replace(dot, ".");
+            }
+        }
+
+        return arg;
+    }
+
+
     public static String removePeriod(String arg) {
 
-        if(arg.split("")[0].equals(".")) {
-
-            arg = arg.substring(1);
-
+        if(arg.startsWith(".")) {
+            arg = arg.substring(1, arg.length());
         }
-        if(arg.split("")[arg.length()-1].equals(".")) {
 
+        if(arg.endsWith(".")) {
             arg = arg.substring(0, arg.length()-1);
         }
 
         return arg;
     }
 
-    public static String lengChk(String answer) {
+    public static String lengChk(String arg) {
 
-        if(answer.length() >= 16) {
+        if(arg.length() >= 16) {
 
-            answer = answer.substring(0, 15);
-            answer = removePeriod(answer);
+            arg = arg.substring(0, 15);
+            arg = removePeriod(arg);
 
-        } else if(answer.length() <= 2) {
+        } else if(arg.length() <= 2) {
 
             for(int i=0; i<3; i++) {
 
-                answer += answer.charAt(answer.length() - 1);
+                arg += arg.charAt(arg.length() - 1);
 
-                if(answer.length() > 2) {
-                    return answer;
+                if(arg.length() > 2) {
+                    return arg;
                 }
             }
         }
 
-        return answer;
+        return arg;
     }
 }
 
 /*
 
-no	new_id	                         result
-예1	"...!@BaT#*..y.abcdefghijklm"	 "bat.y.abcdefghi"
-예2	"z-+.^."	                     "z--"
-예3	"=.="	                         "aaa"
-예4	"123_.def"	                     "123_.def"
-예5	"abcdefghijklmn.p"	             "abcdefghijklmn"
-
-"a.$.a" => "a.a"
-
-".................................." => "aaa"
  */
 
 
