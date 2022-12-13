cc9c2f96c4b0054f780ce5ff3dd0a52cc432b909
yangjaehyuk <didwogur28@naver.com>
2022-12-13 17:28:31 +0900

    # feat : Lv2 - 24

--- src/main/java/study/_221212/Lv2_24.java
+++ src/main/java/study/_221212/Lv2_24.java
@@ -30,9 +30,15 @@ public class Lv2_24 {
     }
 
     public static int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
         for(int i=0; i<arr1.length; i++) {
-            
+            for(int j=0; j<arr2[0].length; j++) {
+                for(int k=0; k<arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+
         }
         return answer;
     }
@@ -40,8 +46,6 @@ public class Lv2_24 {
 
 /*
 
-[[1, 4], [3, 2], [4, 1]]	        [[3, 3], [3, 3]]	                [[15, 15], [15, 15], [15, 15]]
-[[2, 3, 2], [4, 2, 4], [3, 1, 4]]	[[5, 4, 3], [2, 4, 1], [3, 1, 1]]	[[22, 22, 11], [36, 28, 18], [29, 20, 14]]
 
  */
 
