0edb32715f219e6a8f3f7b9e0640736a5a945539
yangjaehyuk <didwogur28@naver.com>
2023-01-26 13:23:51 +0900

    # feat : Lv2 - 40

--- src/main/java/study/_230120/Lv2_40.java
+++ src/main/java/study/_230120/Lv2_40.java
@@ -75,12 +75,6 @@ public class Lv2_40 {
 
 /*
 
-3	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	                        50
-3	["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]	                                21
-2	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	60
-5	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"]	52
-2	["Jeju", "Pangyo", "NewYork", "newyork"]	                                                                        16
-0	["Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	                                                                    25
 
  */
 
